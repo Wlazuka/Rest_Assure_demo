bdbccb251ec047b98b85158f5892f1040b433c1c
Wojciech Lazuka <wlazuka@akamai.com>
2022-11-24 12:15:06 +0100

    Fix PropertyManager

--- src/main/java/com/jsonplaceholder/endpoints/BaseEndpoint.java
+++ src/main/java/com/jsonplaceholder/endpoints/BaseEndpoint.java
@@ -4,11 +4,6 @@ import com.jsonplaceholder.config.PropertyManager;
 
 public class BaseEndpoint {
 
-    public static final String BASE_URL2 = PropertyManager.getProperty("base.url");
-    public static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    public static final String BASE_URL = PropertyManager.getProperty("base.url");
 
-
-    public static void main(String[] args) {
-        System.out.println(BASE_URL2);
-    }
 }
